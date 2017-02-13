3389095df821e706a4e6ac332d245e1e2760f7c0
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2017-02-13 07:30:15 +0100

    404 error handling.

---
+++ src/main/java/fr/scolomfr/recette/controller/error/NotFoundControllerAdvice.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ * Scolomfr Recette
+ * 
+ * Copyright (C) 2017  MENESR (DNE)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package fr.scolomfr.recette.controller.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * Global exception handler related to
+ * <param-name>throwExceptionIfNoHandlerFound</param-name> in web.xml
+ */
+@ControllerAdvice
+public class NotFoundControllerAdvice {
+
+	/**
+	 * Dispays specific "not found" page.
+	 * 
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handle(Exception ex) {
+		return new ModelAndView("not-found");
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/not-found.jsp
@@ -0,0 +1,25 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<t:layout>
+	<jsp:body>
+	
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">Désolé</h1>
+				</div>
+				<!-- /.col-lg-12 -->
+			</div>
+			<!-- /.row -->
+			<div class="row">
+				La page n'a pas été trouvée.
+			</div>
+			<!-- /.row -->
+	
+        
+    </jsp:body>
+</t:layout>
+
+
+
